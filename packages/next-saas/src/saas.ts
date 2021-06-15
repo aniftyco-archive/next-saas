09ace2d30739692a9040a1cbc39b585ec3ebb45b
Josh Manders <josh@joshmanders.com>
2021-06-14 19:35:01 -0500

    add build command back

--- packages/next-saas/src/saas.ts
+++ packages/next-saas/src/saas.ts
@@ -23,6 +23,7 @@ export type cliCommand = (argv?: string[]) => void;
 const commands: { [command: string]: () => Promise<cliCommand> } = {
   start: () => import('next/dist/cli/next-start').then((i) => i.nextStart),
   dev: () => import('next/dist/cli/next-dev').then((i) => i.nextDev),
+  build: () => import('next/dist/cli/next-build').then((i) => i.nextBuild),
   telemetry: () => import('next/dist/cli/next-telemetry').then((i) => i.nextTelemetry),
   worker: () => import('./cli/saas-worker').then((i) => i.saasWorker),
 };
