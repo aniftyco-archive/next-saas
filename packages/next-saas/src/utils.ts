f31a47ae6493f358e97a0e063e3e14d84b1f0a45
Josh Manders <josh@joshmanders.com>
2021-08-04 15:15:24 -0500

    add missing help output

--- packages/next-saas/src/utils.ts
+++ packages/next-saas/src/utils.ts
@@ -1,6 +1,21 @@
+import { printAndExit } from 'next/dist/server/lib/utils';
 import chalk from 'chalk';
 
 const help = {
+  build: `
+  Description
+    Compiles the application for production deployment
+
+  Usage
+    $ saas build <dir>
+
+  <dir> represents the directory of the Next.js application.
+  If no directory is provided, the current directory will be used.
+  Options
+
+  --profile     Can be used to enable React Production Profiling
+  --no-lint     Disable linting
+  `,
   start: `
   Description
     Starts the application in production mode.
@@ -43,9 +58,56 @@ const help = {
 
   Learn more: ${chalk.cyan('https://nextjs.org/telemetry')}
 `,
+  lint: `
+  Description
+    Run ESLint on every file in specified directories. 
+    If not configured, ESLint will be set up for the first time.
+
+  Usage
+    $ saas lint <baseDir> [options]
+
+  <baseDir> represents the directory of the Next.js application.
+  If no directory is provided, the current directory will be used.
+
+  Options
+    Basic configuration:
+      -h, --help                      List this help
+      -d, --dir Array                 Set directory, or directories, to run ESLint - default: 'pages', 'components', and 'lib'
+      -c, --config path::String       Use this configuration file, overriding all other config options
+      --ext [String]                  Specify JavaScript file extensions - default: .js, .jsx, .ts, .tsx
+      --resolve-plugins-relative-to path::String  A folder where plugins should be resolved from, CWD by default
+
+    Specifying rules:
+      --rulesdir [path::String]      Use additional rules from this directory
+    
+    Fixing problems:
+      --fix                          Automatically fix problems
+      --fix-type Array               Specify the types of fixes to apply (problem, suggestion, layout)
+    
+    Ignoring files:
+      --ignore-path path::String     Specify path of ignore file
+      --no-ignore                    Disable use of ignore files and patterns
+    
+    Handling warnings:
+      --quiet                        Report errors only - default: false
+      --max-warnings Int             Number of warnings to trigger nonzero exit code - default: -1
+    
+    Output:
+      -f, --format String            Use a specific output format - default: Next.js custom formatter
+    
+    Inline configuration comments:
+      --no-inline-config             Prevent comments from changing config or rules
+      --report-unused-disable-directives  Adds reported errors for unused eslint-disable directives ("error" | "warn" | "off")
+    
+    Caching:
+      --cache                        Only check changed files - default: false
+      --cache-location path::String  Path to the cache file or directory - default: .eslintcache
+    
+    Miscellaneous:
+      --error-on-unmatched-pattern   Show errors when any file patterns are unmatched - default: false
+`,
 };
 
 export const displayHelpForNextCommand = (command: string) => {
-  console.log(help[command]);
-  process.exit(0);
+  printAndExit(help[command], 0);
 };
