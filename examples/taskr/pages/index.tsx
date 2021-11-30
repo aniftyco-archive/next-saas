b497f6265c3673d784f6401713838c2139ba86e6
Josh Manders <josh@joshmanders.com>
2021-11-29 22:56:40 -0600

    revert back to old return

--- examples/taskr/pages/index.tsx
+++ examples/taskr/pages/index.tsx
@@ -2,15 +2,13 @@ import { Fragment } from 'react';
 import Head from 'next/head';
 import { HelloWorld } from 'components/HelloWorld';
 
-const IndexPage = () => {
-  return (
-    <Fragment>
-      <Head>
-        <title>Next SaaS Starter</title>
-      </Head>
-      <HelloWorld />
-    </Fragment>
-  );
-};
+const IndexPage = () => (
+  <Fragment>
+    <Head>
+      <title>Next SaaS Starter</title>
+    </Head>
+    <HelloWorld />
+  </Fragment>
+);
 
 export default IndexPage;
