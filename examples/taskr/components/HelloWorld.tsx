bfbeb902bdb163e86c316686a29d510d4bb69b9e
Josh Manders <josh@joshmanders.com>
2021-11-29 22:51:51 -0600

    revert socket io stuff

--- examples/taskr/components/HelloWorld.tsx
+++ examples/taskr/components/HelloWorld.tsx
@@ -1,7 +1,9 @@
 import { FunctionComponent } from 'react';
 
-export const HelloWorld: FunctionComponent = ({ children }) => (
+export const HelloWorld: FunctionComponent = () => (
   <div className="flex flex-col items-center justify-center h-screen">
-    <h2 className="p-3 text-lg font-bold text-white bg-purple-700 rounded-lg md:text-2xl">{children}</h2>
+    <h2 className="p-3 text-lg font-bold text-white bg-purple-700 rounded-lg md:text-2xl">
+      Welcome to your Next SaaS Starter Site!
+    </h2>
   </div>
 );
--- examples/taskr/pages/index.tsx
+++ examples/taskr/pages/index.tsx
@@ -1,23 +1,14 @@
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment } from 'react';
 import Head from 'next/head';
 import { HelloWorld } from 'components/HelloWorld';
-import socketClient from 'socket.io-client';
 
 const IndexPage = () => {
-  const [message, setMessage] = useState('Welcome to your Next SaaS Starter Site!');
-
-  useEffect(() => {
-    const socket = socketClient();
-
-    socket.on('message', setMessage);
-  }, []);
-
   return (
     <Fragment>
       <Head>
         <title>Next SaaS Starter</title>
       </Head>
-      <HelloWorld>{message}</HelloWorld>
+      <HelloWorld />
     </Fragment>
   );
 };
