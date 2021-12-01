c182dc3ee13ae22d1e12d1250549a12ccaf34098
Josh Manders <josh@joshmanders.com>
2021-11-30 18:34:02 -0600

    update to node 16 types

--- packages/next-saas/src/prisma.ts
+++ packages/next-saas/src/prisma.ts
@@ -1,18 +1,13 @@
+/* eslint-disable no-var */
 import { PrismaClient } from '@prisma/client';
 
-// add prisma to the NodeJS global type
-interface CustomNodeJsGlobal extends NodeJS.Global {
-  prisma: PrismaClient;
+// add prisma to the global type
+declare global {
+  var prisma: PrismaClient;
 }
 
 // Prevent multiple instances of Prisma Client in development
-declare const global: CustomNodeJsGlobal;
-
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    rejectOnNotFound: true,
-  });
+export const prisma = global.prisma || new PrismaClient({ rejectOnNotFound: true });
 
 if (process.env.NODE_ENV === 'development') {
   global.prisma = prisma;
