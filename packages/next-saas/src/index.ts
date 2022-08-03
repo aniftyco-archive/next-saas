49d4b79ec4890aab34e44ff9b79c04d7044ca8a3
Josh Manders <josh@joshmanders.com>
2022-08-03 18:51:36 -0500

    add event api back

--- packages/next-saas/src/index.ts
+++ packages/next-saas/src/index.ts
@@ -2,6 +2,7 @@ export * from './api-handler';
 export * from './errors';
 export * as mailer from './mailer';
 export { prisma as db } from './prisma';
+export * as event from './event';
 export { default } from './api-handler';
 
 export * as log from 'next/dist/build/output/log';
